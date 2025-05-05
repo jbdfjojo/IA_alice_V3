e4037b5fc6045bf516808f7dbc05f180adbf3368
jbdfjojo <31694393+jbdfjojo@users.noreply.github.com>
2025-05-05 08:00:21 +0200

    update

--- lancer_alice.bat
+++ lancer_alice.bat
@@ -1,4 +1,5 @@
 @echo off
-:: Lancement via l'Anaconda Prompt
-start "" "%USERPROFILE%\anaconda3\Scripts\activate.bat" ia_camembert && ^
-cmd /k "cd /d %~dp0 && python src\app.py"
+%WINDIR%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command ^
+"& 'C:\Users\Blazufr\anaconda3\shell\condabin\conda-hook.ps1'; ^
+conda activate ia_camembert; ^
+python 'C:\Users\Blazufr\Desktop\IA_alice_V3\src\app.py'"
